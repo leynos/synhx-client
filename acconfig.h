adb1bf8c8cb12f7eb2fa2c01673295a6312f262b
devinteske <>
2001-09-03 18:55:03 +0000

    sf cvs sucks

---
+++ acconfig.h
@@ -0,0 +1,77 @@
+/* Define to `long int' if not defined by system */
+#undef ssize_t
+
+/* Define if not defined by system
+   u_int*_t is an integer type of exactly * bits */
+#undef u_int8_t
+#undef u_int16_t
+#undef u_int32_t
+#undef int8_t
+#undef int16_t
+#undef int32_t
+
+/* Define if snprintf is broken */
+#undef BROKEN_SNPRINTF
+
+/* Define to to use pthreads instead of fork or clone for htxf */
+#undef CONFIG_HTXF_PTHREAD
+
+/* Define to to use clone instead of fork for htxf */
+#undef CONFIG_HTXF_CLONE
+
+/* Define to use fork instead of clone for htxf */
+#undef CONFIG_HTXF_FORK
+
+/* Define to compile image preview code */
+#undef CONFIG_HTXF_PREVIEW
+
+/* Define to enable HFS */
+#undef CONFIG_HFS
+
+/* Define to compile megahal */
+#undef CONFIG_HAL
+
+/* Define to compile HOPE */
+#undef CONFIG_HOPE
+
+/* Define to compile cipher */
+#undef CONFIG_CIPHER
+
+/* Define to compile compress */
+#undef CONFIG_COMPRESS
+
+/* Define if you want sql logging */
+#undef CONFIG_SQL
+
+/* Define if you have the strcasestr function */
+#undef HAVE_STRCASESTR
+
+/* Define if you have the basename function */
+#undef HAVE_BASENAME
+
+/* Define if you have the realpath function */
+#undef HAVE_REALPATH
+
+/* Define if you have the strptime function */
+#undef HAVE_STRPTIME
+
+/* Path to tr */
+#undef PATH_TR
+
+/* Default sound player */
+#undef DEFAULT_SND_PLAYER
+
+/* Define to compile trackname command */
+#undef CONFIG_XMMS
+
+/* Define to dull the icon of a user when the user is away */
+#undef CONFIG_DULLED
+
+/* Enable IPv6 Support */
+#undef CONFIG_IPV6
+
+/* Define to detect DARWIN users */
+#undef DARWIN
+
+/* Define to detect CARBON API on host */
+#undef CARBON
